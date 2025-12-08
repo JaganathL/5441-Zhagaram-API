6753e0fd64977a39b57c38bdc8e1d846e0f57459
JAGANATH <jaganathjagan123@gmail.com>
2025-12-08 10:45:00 +0530

    Handle CORS configuration

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
